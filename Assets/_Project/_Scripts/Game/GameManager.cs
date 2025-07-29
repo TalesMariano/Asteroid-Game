2292efbeacbb1fc3e0c227a42664baa858739039
Tales Mariano <tales.mariano@magicmedia.studio>
2025-07-29 15:21:30 -0300

    Add Gama Manager

---
+++ Assets/_Project/_Scripts/Game/GameManager.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton Pattern
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] 
+    private SO_GameParameters soGameParameters;
+    [SerializeField, Tooltip("Used when SO is null")] 
+    private GameParameters _debugGameParameters;
+
+    private GameParameters Parameters
+    {
+        get { return soGameParameters ? soGameParameters.gameParameters : _debugGameParameters; }
+    }
+
+
+
+    [Header("References")]
+    [SerializeField] private AsteroidSpawner _asteroidSpawner;
+    [SerializeField] private ShipController _shipPrefab;
+
+    [SerializeField] private int _playerLives;
+    public int PlayerLives
+    {
+        get { return _playerLives; }
+        set {
+            _playerLives = value;
+            OnPlayerLivesChange?.Invoke(_playerLives);
+        }
+    }
+
+
+
+    public int Score { get; private set; }
+
+    public Action<int> OnScore;
+    public Action<int> OnPlayerLivesChange;
+
+    public Action OnGameStart;
+    public Action OnGameOver;
+
+
+    private HashSet<Asteroid> _activeAsteroids = new HashSet<Asteroid>();
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+
+    public void AddScore(int points)
+    {
+        Score += points;
+        OnScore?.Invoke(Score);
+    }
+
+    [ContextMenu("StartGame")]
+    public void StartGame()
+    {
+        PlayerLives = Parameters.playerLives;
+
+        _asteroidSpawner.Spawn();
+        SpawnPlayerShip();
+        Debug.Log("Game Start");
+    }
+
+    [ContextMenu("SpawnPlayer")]
+    void SpawnPlayerShip()
+    {
+        Vector3 spanwLocation = Vector3.zero;
+        ShipController ship =  Instantiate(_shipPrefab, spanwLocation, Quaternion.identity);
+
+        ship.OnDestroyed += PlayerHit;
+    }
+
+
+    [ContextMenu("PlayerHit")]
+    void PlayerHit()
+    {
+        PlayerLives--;
+        if (PlayerLives > 0)
+        {
+            StopAllCoroutines();    // To prevent multiple player ships
+            StartCoroutine(WaitRespawn(1));
+        }
+        else
+        {
+            Debug.Log("GameOver");
+            OnGameOver?.Invoke();
+        }
+    }
+
+    public void AsteroidCreated(Asteroid asteroid)
+    {
+        _activeAsteroids.Add(asteroid);
+    }
+
+    public void AsteroidDestroied(Asteroid asteroid)
+    {
+        _activeAsteroids.Remove(asteroid);
+        CheckGameEnd();
+    }
+
+    void CheckGameEnd()
+    {
+        if(_activeAsteroids.Count == 0)
+        {
+            Debug.Log("Game Won");
+        }
+    }
+
+    IEnumerator WaitRespawn(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SpawnPlayerShip();
+    }
+}
+
+public enum GameState
+{
+    Intro,
+    Gameplay,
+    GameOver,
+    GameWon
+}
