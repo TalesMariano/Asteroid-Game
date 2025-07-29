4fde3b8bb103e5ce9c758d146594d9c51e5adf75
Tales Mariano <tales.mariano@magicmedia.studio>
2025-07-29 17:53:56 -0300

    Add Scene Reload, Code Clean Up, and Update parameters

--- Assets/_Project/_Scripts/Game/GameManager.cs
+++ Assets/_Project/_Scripts/Game/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -81,6 +82,10 @@ public class GameManager : MonoBehaviour
         {
             StartGame();
         }
+
+        // RestartScene
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
         // Quit Game
         if (Input.GetKeyDown(KeyCode.Escape))
